875a026574cbabbebcf53052e209764a36b37bb6
Jan Galinski <jan.galinski@holisticon.de>
2019-02-05 22:55:45 +0100

    use const

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,8 +3,8 @@ import org.gradle.kotlin.dsl.embeddedKotlinVersion
 object Versions {
   val kotlin = embeddedKotlinVersion
 
-  val camunda = "7.10.0"
-  val camundaEE = "7.10.1-ee"
+  const val camunda = "7.10.0"
+  const val camundaEE = "7.10.2-ee"
 
-  val typedValues = "1.6.1"
+  const val typedValues = "1.6.1"
 }
