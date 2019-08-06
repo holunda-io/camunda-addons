27a186a4e32656c60e12918ab0c94f7ad861c6c8
Jan Galinski <jan.galinski@holisticon.de>
2019-08-06 17:59:13 +0200

    camunda 7.11, fixes #11

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,9 +2,13 @@ import org.gradle.kotlin.dsl.embeddedKotlinVersion
 
 object Versions {
   val kotlin = embeddedKotlinVersion
+  val java = "1.8"
 
-  const val camunda = "7.10.0"
-  const val camundaEE = "7.10.5-ee"
+  val springBoot = "2.1.6.RELEASE"
+
+  const val camunda = "7.11.0"
+  const val camundaEE = "7.11.2-ee"
+  const val camundaSpringBoot = "3.3.2"
 
   const val junit5 = "5.4.1"
 
