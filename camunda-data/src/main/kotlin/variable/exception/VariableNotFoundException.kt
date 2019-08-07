06131ada30b3b30d6270911961b503f8a1d04660
Jan Galinski <jan.galinski@holisticon.de>
2019-08-07 21:10:18 +0200

    javadoc

--- camunda-data/src/main/kotlin/variable/exception/VariableNotFoundException.kt
+++ camunda-data/src/main/kotlin/variable/exception/VariableNotFoundException.kt
@@ -3,7 +3,12 @@ package io.holunda.camunda.addons.data.variable.exception
 import io.holunda.camunda.addons.data.variable.Variable
 import java.util.function.Supplier
 
-
+/**
+ * Creates a Supplier of VariableNotFoundException to use with Optional#orElseThrow.
+ */
 fun variableNotFound(variable: Variable<*>) : Supplier<VariableNotFoundException> = Supplier { VariableNotFoundException(variable) }
 
+/**
+ * Raise this if a variable can not be found for given variable.
+ */
 class VariableNotFoundException(variable: Variable<*>) : IllegalStateException("no value found for variable:${variable.key}")
